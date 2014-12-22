92539fa3109be52ece73cbd84ab7281527da6d89
gabi.mabs <gabi.mabs@gmail.com>
2014-12-22 20:31:36 +0000

    [Gab] Changed Sign In Servlet so that it checks the password of the user

--- SopCovTraining/src/java/sopcov/servlet/SignInServlet.java
+++ SopCovTraining/src/java/sopcov/servlet/SignInServlet.java
@@ -6,11 +6,17 @@
 package sopcov.servlet;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import sopcov.models.DBI;
+import sopcov.models.DBManager;
+import sopcov.models.MySQLDBI;
 
 /**
  *
@@ -29,7 +35,68 @@ public class SignInServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("/userPage.jsp");
+
+        String destination = "index.jsp";
+        //String destination = "userPage.jsp";
+
+        //retreive the email and the pswd
+        HttpSession s = request.getSession();
+        String email = null;
+        String pswd = null;
+        if (s != null && !s.isNew()) {
+            email = (String) s.getAttribute("email");
+            pswd = (String) s.getAttribute("pswd");
+        }
+        if (email != null && !email.isEmpty() && pswd != null && !pswd.isEmpty()) {
+
+            //retreive the database informations from the context
+            String dbname = getServletContext().getInitParameter("dbuserid");
+            String dbpswd = getServletContext().getInitParameter("dbuserpswd");
+            String dbcatalog = getServletContext().getInitParameter("dbcatalog");
+
+            //set the dbi for mysql
+            DBI dbi = new MySQLDBI(dbname, dbpswd, dbcatalog);
+            System.out.println(dbi.getConnectionDetails());
+            System.out.println(dbi.getConnectionURL());
+
+            DBManager dbm = new DBManager(dbi);
+
+            if (!dbm.isConnected()) {
+                //TO-DO redirect to sign in page with an error message            
+                System.out.println("In SignInServlet : database is not connected, connecting it...");
+                if (!dbm.openConnection()) {
+                    System.err.println("Error in SignInServlet : could not connect to the database");
+                }
+                System.out.println("In SignInServlet : connection status : " + dbm.isConnected());
+
+            }
+
+            if (!dbm.isConnected()) {
+                System.out.println("Error in SignInServlet : could not connect to the DB");
+            } else {
+                try {
+                    //Creating the query
+                    String query = "select pswd from utilisateur where email = '" + email + "'";
+
+                    //Sending the query 
+                    // /!\ should be done by the model actually
+                    ResultSet res = dbm.ExecuteResultSet(query);
+                    res.next();
+                    
+                    //Testing if the password match, normally there is just one row
+                    if (res.getString("pswd").equals(pswd)) {
+                        System.out.println("In SignInServlet : Password match");
+                        destination = "userPage.jsp";
+                    } else {
+                        System.out.println("In SignInServlet : Password do not match");
+                    }
+                } catch (SQLException ex) {
+                    System.err.println("Error in SignInServlet : " + ex.getLocalizedMessage());
+                }
+            }
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/" + destination);
         rd.forward(request, response);
     }
 
