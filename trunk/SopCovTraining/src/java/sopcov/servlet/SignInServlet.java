6f05894b0e6c3d2a217d955baa5d3c007cc60d98
gabi.mabs@gmail.com <gabi.mabs@gmail.com@f2714585-b9d4-1075-7f11-937478e4fa2d>
2014-12-22 15:42:34 +0000

    [Gab] Tried to reimplement what I already did using jsp pages instead of doing everything in the servlets

--- trunk/SopCovTraining/src/java/sopcov/servlet/SignInServlet.java
+++ trunk/SopCovTraining/src/java/sopcov/servlet/SignInServlet.java
@@ -6,7 +6,7 @@
 package sopcov.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,27 +29,8 @@ public class SignInServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SignInServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Sign In Page</h1>");
-            out.println("<p>");
-            out.println("Email is : ");
-            out.println(request.getAttribute("email"));
-            out.println("<br/>");
-            out.println("Password is : ");
-            out.println(request.getAttribute("password"));
-            out.println("<br/>");
-            out.println("</p>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        RequestDispatcher rd = request.getRequestDispatcher("/userPage.jsp");
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
