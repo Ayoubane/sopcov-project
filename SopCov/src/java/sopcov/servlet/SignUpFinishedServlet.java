39bef98b6a518e63cc28d8e69b14902f77ce04a6
gabi.mabs <gabi.mabs@gmail.com>
2015-01-01 16:08:59 +0000

    [Gab] SignUpFinishedServlet : Maintenant possible d'ajouter un utilisateur à la base de données. La vérification des paramètres n'est pas encore faite.

--- SopCov/src/java/sopcov/servlet/SignUpFinishedServlet.java
+++ SopCov/src/java/sopcov/servlet/SignUpFinishedServlet.java
@@ -8,6 +8,7 @@ package sopcov.servlet;
 import database.DB;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,48 +33,50 @@ public class SignUpFinishedServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            //Récupère la session
-            HttpSession s = request.getSession();
-            //On se munit du DB helper :
-            DB dbi = new DB();
-            //Récupère tous les parametres nécessaire à la création de l'utilisateur
-            boolean admin = false;
-            //Depuis la session
-            String email = (String) s.getAttribute("email");
-            String password = (String) s.getAttribute("password");
-            //Depuis la requête            
-            String nom = (String) request.getParameter("nom");
-            String prenom = (String) request.getParameter("prenom");
-            String tel = (String) request.getParameter("tel");
-            String adresse = (String) request.getParameter("adresse");
-            String commune = (String) request.getParameter("commune");
-            String codePostal = (String) request.getParameter("code_postal");
-            String lieuTravail = (String) request.getParameter("lieu_travail");
-            String heureDepart = (String) request.getParameter("heure_depart");
-            String heureRetour = (String) request.getParameter("heure_retour");
+
+        String destination = "index.jsp";
+        /* TODO output your page here. You may use following sample code. */
+        //Récupère la session
+        HttpSession s = request.getSession();
+        //On se munit du DB helper :
+        DB dbi = new DB();
+        //Récupère tous les parametres nécessaire à la création de l'utilisateur
+        boolean admin = false;
+        //Depuis la session
+        String email = (String) s.getAttribute("email");
+        String password = (String) s.getAttribute("password");
+        //Depuis la requête            
+        String nom = (String) request.getParameter("nom");
+        String prenom = (String) request.getParameter("prenom");
+        String tel = (String) request.getParameter("tel");
+        String adresse = (String) request.getParameter("adresse");
+        String commune = (String) request.getParameter("commune");
+        String codePostal = (String) request.getParameter("code_postal");
+        String lieuTravail = (String) request.getParameter("lieu_travail");
+        String heureDepart = (String) request.getParameter("heure_depart");
+        String heureRetour = (String) request.getParameter("heure_retour");
 
             //Récupère les jours de travail de la requete
-            //Lundi Mardi Mercredi devient Lun,Mar,Mer
-            String joursTravail = "";
-            String[] joursRequete = request.getParameterValues("jours_travail");
-            if (joursRequete != null) {
-                for (int i = 0; i < joursRequete.length; i++) {
-                    //On récupére les trois première lettre
-                    joursTravail = joursTravail + joursRequete[i].substring(0, 3);
-                    //On met une virgule si c'est pas le dernier string
-                    if (i != joursRequete.length - 1) {
-                        joursTravail = joursTravail + ",";
-                    }
+        //Lundi Mardi Mercredi devient Lun,Mar,Mer
+        String joursTravail = "";
+        String[] joursRequete = request.getParameterValues("jours_travail");
+        if (joursRequete != null) {
+            for (int i = 0; i < joursRequete.length; i++) {
+                //On récupére les trois première lettre
+                joursTravail = joursTravail + joursRequete[i].substring(0, 3);
+                //On met une virgule si c'est pas le dernier string
+                if (i != joursRequete.length - 1) {
+                    joursTravail = joursTravail + ",";
                 }
             }
+        }
 
-            //Si il y a quelque chose ici c'est forcément on...
-            boolean conducteur = request.getParameter("conducteur") != null;
-            boolean notif = request.getParameter("notif") != null;
+        //Si il y a quelque chose ici c'est forcément on...
+        boolean conducteur = request.getParameter("conducteur") != null;
+        boolean notif = request.getParameter("notif") != null;
 
-            PrintWriter pw = response.getWriter();
+        /*
+        try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -81,26 +84,50 @@ public class SignUpFinishedServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            pw.println("<p>");
-            pw.println("Paramètres entrés : <br/>");
-            pw.println("email: " + email + "<br/>");
-            pw.println("password: " + password + "<br/>");
-            pw.println("nom: " + nom + "<br/>");
-            pw.println("prenom: " + prenom + "<br/>");
-            pw.println("tel: " + tel + "<br/>");
-            pw.println("adresse: " + adresse + "<br/>");
-            pw.println("commune: " + commune + "<br/>");
-            pw.println("codePostal: " + codePostal + "<br/>");
-            pw.println("lieuTravail: " + lieuTravail + "<br/>");
-            pw.println("heureDepart: " + heureDepart + "<br/>");
-            pw.println("heureRetour: " + heureRetour + "<br/>");
-            pw.println("joursTravail: " + joursTravail + "<br/>");
-            pw.println("conducteur: " + conducteur + "<br/>");
-            pw.println("notif: " + notif + "<br/>");
-            pw.println("</p>");
+            out.println("<p>");
+            out.println("Paramètres entrés : <br/>");
+            out.println("email: " + email + "<br/>");
+            out.println("password: " + password + "<br/>");
+            out.println("nom: " + nom + "<br/>");
+            out.println("prenom: " + prenom + "<br/>");
+            out.println("tel: " + tel + "<br/>");
+            out.println("adresse: " + adresse + "<br/>");
+            out.println("commune: " + commune + "<br/>");
+            out.println("codePostal: " + codePostal + "<br/>");
+            out.println("lieuTravail: " + lieuTravail + "<br/>");
+            out.println("heureDepart: " + heureDepart + "<br/>");
+            out.println("heureRetour: " + heureRetour + "<br/>");
+            out.println("joursTravail: " + joursTravail + "<br/>");
+            out.println("conducteur: " + conducteur + "<br/>");
+            out.println("notif: " + notif + "<br/>");
+            out.println("</p>");
             out.println("</body>");
             out.println("</html>");
         }
+        */
+
+        int creationMarche = dbi.addNewUser(admin, prenom, nom, password, tel, email, adresse, commune, codePostal, "", heureDepart, heureRetour, joursTravail, conducteur, notif);
+
+        switch (creationMarche) {
+            case 0:
+                destination = "userPage.jsp";
+                break;
+            case -1:
+                s.setAttribute("msgErreur", "Création du profil impossible. Votre lieu de travail n'a pas été trouvé dans la base de données.");
+                break;
+            case -2:
+                s.setAttribute("msgErreur", "Création du profil impossible. Problème d'acces à la base de données quand on a cherché votre lieu de travail");
+                break;
+            case -3:
+                s.setAttribute("msgErreur", "Création du profil impossible. Problème d'acces à la base de données quand nous avons essayé de créer votre profil");
+                break;
+            default:
+                s.setAttribute("msgErreur", "Problème lors de la création de votre profil");
+                break;
+        }
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/" + destination);
+        rd.forward(request, response);
     }
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
