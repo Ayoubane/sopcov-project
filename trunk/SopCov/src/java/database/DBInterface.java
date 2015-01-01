91ed6fe584cd9c5bf396b66f805bf136e6f03ce5
gabi.mabs@gmail.com <gabi.mabs@gmail.com@f2714585-b9d4-1075-7f11-937478e4fa2d>
2015-01-01 16:07:26 +0000

    [Gab] DB pour ajouter un utilisateur je n'ai pas accès à l'ID du lieu de travail, j'ai donc copié la fonction d'Omar à laquelle j'ai ajouté une query pour le récupérer.

--- trunk/SopCov/src/java/database/DBInterface.java
+++ trunk/SopCov/src/java/database/DBInterface.java
@@ -25,6 +25,7 @@ public interface DBInterface {
     public List<User> queryInfo(String email);
     public String printHTML() ;
     public int addNewUser(int admin, String prenom, String nom, String password, String tel, String email, String adresse, String commune, int code_postal, int lieu_travail_id, String heure_depart, String heure_retour, String jours_travail, int conducteur, int notif );
+    public int addNewUser(boolean admin, String prenom, String nom, String password, String tel, String email, String adresse, String commune, String codePostal, String nomLieuTravail, String heureDepart, String heureRetour, String joursTravail, boolean conducteur, boolean notif );
     public void deleteUser(String email, String prenom, String nom) ;
     public String getPassword(String email);
     public boolean editLocation(String email, int lieu_travail_id);
