0934e960bbd71d3bd0c16eb0701fbaf082c9e73b
gabi.mabs@gmail.com <gabi.mabs@gmail.com@f2714585-b9d4-1075-7f11-937478e4fa2d>
2015-01-07 08:48:53 +0000

    [Gab] Enlevé le import de trop

--- trunk/SopCov/src/java/sopcov/servlet/EditProfileServlet.java
+++ trunk/SopCov/src/java/sopcov/servlet/EditProfileServlet.java
@@ -10,11 +10,7 @@ import database.DB;
 import database.DBInterface;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,7 +18,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import sopcov.models.*;
 
 
 /**
