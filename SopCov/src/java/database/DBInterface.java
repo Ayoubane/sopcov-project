3cda3975082fe1e3bb5641e0c95a6c9eea4e2716
gabi.mabs <gabi.mabs@gmail.com>
2015-01-07 09:59:46 +0000

    [Gab] 	DBInterface : 	Ajout d'une nouvelle fonction pour l'administrateur (getNumberOfConnectionBetween) 	DB : 	Implementation de la fonction getNumberOfConnectionBetween 		Creation d'un petit jeu de test avec la base de donnée de base pour cette fonction

--- SopCov/src/java/database/DBInterface.java
+++ SopCov/src/java/database/DBInterface.java
@@ -25,7 +25,33 @@ public interface DBInterface {
     public List<User> queryInfo(String email);
     public String printHTML() ;
     public int addNewUser(int admin, String prenom, String nom, String password, String tel, String email, String adresse, String commune, int code_postal, int lieu_travail_id, String heure_depart, String heure_retour, String jours_travail, int conducteur, int notif );
+    
+    /**
+     * Ajoute un utilisateur à la base de donnée.
+     * Attention aucune vérification des paramètres n'est faite.
+     * 
+     * @param admin Si l'utilisateur doit être administrateur ou non
+     * @param prenom Le prenom de l'utilisateur
+     * @param nom Le nom de l'utilisateur
+     * @param password Le nom de l'utilisateur
+     * @param tel Le numéro de téléphone de l'utilisateur
+     * @param email L'email de l'utilisateur
+     * @param adresse L'adresse de l'utilisateur
+     * @param commune La commune de l'utilisateur ("Toulouse")
+     * @param codePostal Le code postal de l'utilisateur ("31100")
+     * @param nomLieuTravail Le nom du lieu de travail de l'utilisateur ("Sopra Group 1")
+     * @param heureDepart L'heure de départ de l'utilisateur ("08:00:00")
+     * @param heureRetour L'heure de retour de l'utilisateur ("17:45:00")
+     * @param joursTravail Les jours de travail de l'utilisateur ("Lun,Mar,Mer,Jeu,Ven,Sam,Dim") ("Mar,Jeu")
+     * @param conducteur Si l'utilisateur est un conducteur qui donc propose le covoiturage ou non
+     * @param notif Si l'utilisateur veut recevoir des mail ou non
+     * @return  0 = tout va bien <br/>
+     *          -1 = n'a pas pu récupérer l'ID associé au lieu de travail<br/>
+     *          -2 = n'a pas pu accéder à l'id du lieu de travail<br/>
+     *          -3 = n'a pas pu créer l'utilisateur
+     */
     public int addNewUser(boolean admin, String prenom, String nom, String password, String tel, String email, String adresse, String commune, String codePostal, String nomLieuTravail, String heureDepart, String heureRetour, String joursTravail, boolean conducteur, boolean notif );
+    
     public void deleteUser(String email, String prenom, String nom) ;
     public String getPassword(String email);
     public boolean editLocation(String email, int lieu_travail_id);
@@ -36,4 +62,19 @@ public interface DBInterface {
     public List<User> getAllDrivers();
     public List<User> searchRoute(String mCity,String mWorkplace);
     public ArrayList<String> getAllWorkplaces();
+    
+    /*
+        ADMIN REPORTS :
+    */
+    /**
+     * Renvoie le nombre de connections ayant eu lieu entre les dates dateDeb
+     * heureDeb et dateFin heureFin
+     *
+     * @param dateDeb "AAAA-MM-JJ"
+     * @param heureDeb "HH:MM:SS"
+     * @param dateFin "AAAA-MM-JJ"
+     * @param heureFin "HH:MM:SS"
+     * @return le nombre de connections ou -1 si quelque chose s'est mal passé.
+     */
+    public int getNumberOfConnectionBetween(String dateDeb, String heureDeb, String dateFin, String heureFin);
 }
