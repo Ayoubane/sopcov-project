e165cc159c0ce25ec4bce0e60b553b193936811c
gabi.mabs <gabi.mabs@gmail.com>
2015-01-10 18:29:37 +0000

    [Gab]  GetReportServlet reçoit une requête de rapport de l'administrateur et renvoie la réponse. 	management.jsp est la page de l'administrateur 	SignOutServlet permet le logout et redirige vers la page d'acceuil 	userWelcome.jsp a été modifiée pour accepter un administrateur /!\ faudra changer le boolean admin dans le code java.

---
+++ SopCov/src/java/sopcov/servlet/SignOutServlet.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sopcov.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author gb
+ */
+public class SignOutServlet extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //First let's invalidate the session
+        HttpSession s = request.getSession();
+        if (s!=null) {
+            s.invalidate();
+        }
+        //Then let's forward to the main page
+        String destination = "index.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(destination);
+        rd.forward(request, response);
+    }
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Invalidates a session";
+    }// </editor-fold>
+
+}
